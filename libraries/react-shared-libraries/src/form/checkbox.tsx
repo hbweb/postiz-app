f4909b6346e4ee48d437a0ccdf833e018c96df2b
Hung Bui <hung@quaycreative.com>
2026-07-15 11:11:30 +0700

    Revert checkbox

--- libraries/react-shared-libraries/src/form/checkbox.tsx
+++ libraries/react-shared-libraries/src/form/checkbox.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { FC, forwardRef, useCallback } from 'react';
+import { FC, forwardRef, useCallback, useState } from 'react';
 import clsx from 'clsx';
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
 export const Checkbox = forwardRef<
   null,
   {
@@ -47,6 +47,7 @@ export const Checkbox = forwardRef<
     <div className="flex gap-[10px]">
       <div
         ref={ref}
+        {...disableForm ? {} : form.register(props.name!)}
         onClick={changeStatus}
         className={clsx(
           'cursor-pointer rounded-[4px] select-none w-[24px] h-[24px] justify-center items-center flex text-white',
